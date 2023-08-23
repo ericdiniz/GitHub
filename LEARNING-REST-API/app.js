1eb8fbb75b68ffa697fb6d7223085799a85dd3e5
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-08-23 10:06:31 -0300

    nodemon, morgan, error exception

--- LEARNING-REST-API/app.js
+++ LEARNING-REST-API/app.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express();
 const produtosRouter = require('./routes/produtos')
 const pedidosRouter = require('./routes/pedidos')
 
+
+app.use(morgan('dev'));
 app.use('/teste', (req, res, next) => {
     res.status(200).send('deu bom')
 })
 app.use('/produtos', produtosRouter);
 app.use('/pedidos', pedidosRouter);
+
+//ROTA PARA ERRO
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404
+    next(erro);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+})
 
 module.exports = app;
